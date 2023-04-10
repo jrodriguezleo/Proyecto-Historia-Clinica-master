9a94f69c9a1d6bef441f1e6fd232a9ab9ee9dd6d
Sebastian Rodriguez Leon <103054718+jrodriguezleo@users.noreply.github.com>
2023-04-09 21:49:31 -0500

    Update Person.java
    Pequeños ajustes sobre la lectura

--- src/main/java/Users/Person.java
+++ src/main/java/Users/Person.java
@@ -26,55 +26,42 @@ public abstract class Person extends User{
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
-
     public String getLastName() {
         return lastName;
     }
-
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
-
     public String getBirthdate() {
         return birthdate;
     }
-
     public void setBirthdate(String birthdate) {
         this.birthdate = birthdate;
     }
-
     public String getGender() {
         return gender;
     }
-
     public void setGender(String gender) {
         this.gender = gender;
     }
-
     public String getAddress() {
         return address;
     }
-
     public void setAddress(String address) {
         this.address = address;
     }
-
     public String getPhone() {
         return phone;
     }
-
     public void setPhone(String phone) {
         this.phone = phone;
     }
-
     public String getEmail() {
         return email;
     }
-
     public void setEmail(String email) {
         this.email = email;
     }
